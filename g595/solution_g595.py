a5f24dd3076e7d479d3d418c3d67a06e4f3c7f4c
Roger Lo <sobadrush@icloud.com>
2025-09-17 20:18:47 +0800

    [feat](g595): 新增 g595 的解法
    此解法根據題目要求，計算補齊損壞圍籬(高度為0)所需的總和。
    
    - 遍歷圍籬高度陣列。
    - 當遇到高度為 0 的圍籬時，取其左右相鄰圍籬中的較小值進行累加。
    - 處理陣列邊界情況。

---
+++ g595/solution_g595.py
@@ -0,0 +1,18 @@
+n = int(input("輸入圍籬數量："))
+
+rods = list(map(int, input("輸入圍籬高度: ").split()))
+
+print(f"rods = {rods}")
+
+sum_of_fixed_rod = 0
+for i in range(n):
+  if i == 0:
+    sum_of_fixed_rod += rods[i + 1]
+    continue
+  if i == (n - 1):
+    sum_of_fixed_rod += rods[i - 1]
+    continue
+  if rods[i] == 0:
+    sum_of_fixed_rod += min(rods[i - 1], rods[i + 1])
+
+print(f"sum_of_fixed_rod = {sum_of_fixed_rod}")
