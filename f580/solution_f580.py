9fe3ff40a104b5bd730be6894e5a65d0395a3e3d
Roger Lo <sobadrush@icloud.com>
2025-09-18 13:39:11 +0800

    [refactor](f580): 調整變數命名與修正邏輯錯誤
    - 將變數 m, n 的意義對調，使其符合題目描述 (n: 骰子個數, m: 操作次數)
    
    - 修正 for 迴圈的迭代次數，從 n 改為 m
    
    - 修正 dices 陣列的初始化長度，從 m 改為 n
    
    - 增加註解說明操作方式 else 區塊的用途

--- f580/solution_f580.py
+++ f580/solution_f580.py
@@ -1,25 +1,25 @@
-m, n = map(int, input("請輸入 m (骰子個數), n(操作次數): ").split())
-print(f"m = {m}, n = {n}")
+n, m = map(int, input("請輸入 n (骰子個數), m(操作次數): ").split())
+print(f"n = {n}, m = {m}")
 
 handles = []
-for t in range(n):
+for t in range(m):
   perHandle = tuple(map(int, input("輸入 a (第幾顆骰子) , b (操作方式): ").split()))
   handles.append(perHandle)
 
 print(f"handles = {handles}")
 
-dices = [ [4, 1, 2] for ii in range(m) ] # m: 骰子個數
+dices = [ [4, 1, 2] for ii in range(n) ] # m: 骰子個數
 print(f"dices = {dices}")
 
 for hd in handles:
   target_dice = hd[0] - 1 # 陣列 index 從 0 開始，故多減1
   target_handle = hd[1]
+  tmp_dice = dices[target_dice]
   if target_handle == -1: # 向前轉
-    tmp_dice = dices[target_dice]
     dices[target_dice] = [ tmp_dice[1], 7 - tmp_dice[0], tmp_dice[2] ]
   elif target_handle == -2: # 向右轉
     dices[target_dice] = [ tmp_dice[0], 7 - tmp_dice[2], tmp_dice[1] ]
-  else:
+  else: # 交換位置
     target_handle_idx = target_handle - 1 # 陣列 index 從 0 開始，故多減1
     tmp = dices[target_dice]
     dices[target_dice] = dices[target_handle_idx]
